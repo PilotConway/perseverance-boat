81650872308b3bec8efa89f8a2ab300df064abdf
John L Conway IV <john@johnconwayiv.com>
2013-05-07 21:38:49 -0400

    Initial creation of the rails app. #1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PerseveranceBoat::Application.config.secret_token = '36a4b04925c792d11902121d479df96b2927562d27471cb0d1ff25a4ef2b08faef8d714ecaf0fb81c205604a8e3ae5bdb8cb2efe2184beeedc7baaed35420c2d'
