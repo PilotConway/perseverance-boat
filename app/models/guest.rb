3119eebe7b33bee523cf9dd860b7829654426874
John L Conway IV <john@johnconwayiv.com>
2013-06-16 17:25:53 -0400

    On create of guest a new booking email is sent out #21

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -3,10 +3,18 @@ class Guest < ActiveRecord::Base
   belongs_to :person
   belongs_to :booking
   validate :checkAvailability
+  after_create :booking_notification
 
   def checkAvailability
     if person.over_three and not booking.areSpotsAvailable()
       errors.add(:alert, "There are no spots left on this booking.")
     end
+  end
+
+  # Notify via email that a booking has been created. This email should go to 
+  # perseverance@the-conways.com
+  # We can also notify the guest who booked if they have an email
+  def booking_notification
+    BookingMailer.guest_books(booking, person).deliver
   end
 end
