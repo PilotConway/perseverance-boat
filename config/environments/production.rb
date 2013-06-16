b0a3c3f2b2509b5404d5b8ce16a5b03ffcdc4eae
John L Conway IV <john@johnconwayiv.com>
2013-06-16 18:00:14 -0400

    Added host information for mailers #21

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,8 @@ PerseveranceBoat::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => "perseveranceboat.com" }
+
 
   # Enable threaded mode
   # config.threadsafe!
