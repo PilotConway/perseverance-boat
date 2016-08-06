d917c32bf5506796220ce26262142b789a74d6e7
John L Conway IV <john@riledup.co>
2016-08-06 10:33:11 -0400

    Trying to fix email

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "the-conways.com", 
-  :user_name            => "perseverance@the-conways.com", 
-  :password             => "johnconway", 
-  :authentication       => 'plain', 
+  :domain               => "the-conways.com",
+  :user_name            => "perseverance@the-conways.com",
+  :password             => "u.Q2H2@hf4iU#4FQi}X",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
